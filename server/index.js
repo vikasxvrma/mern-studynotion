a29740ca51029621c3c061ae1651581e4cd8dc1c
Vikas Verma <iamvikas1027@gmail.com>
2026-01-28 19:47:54 +0530

    Fix CORS and proxy for production

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 require("dotenv").config();
 
 const app = express();
+app.set("trust proxy", 1);
+
 const PORT = process.env.PORT || 5000;
 
 // --------------------- Database Connection -------------------
@@ -20,7 +22,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
     credentials: true,
   })
 );
