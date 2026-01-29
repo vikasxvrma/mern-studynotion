1dcc02ee715b8bfe61e85993f147e6e56d6191bd
Vikas Verma <iamvikas1027@gmail.com>
2026-01-29 10:30:13 +0530

    Fix MongoDB TLS issue on Render

--- server/config/database.js
+++ server/config/database.js
@@ -4,17 +4,20 @@
 
 const mongoose = require("mongoose");
 
-require("dotenv").config();
-DATABASE_URL = process.env.DATABASE_URL;
-//  ==============connection with db ===================
-const dbConnect = () => {
+const dbConnect = async () => {
   try {
-    mongoose.connect(DATABASE_URL);
+    await mongoose.connect(process.env.MONGODB_URL, {
+      tls: true,
+      tlsAllowInvalidCertificates: true,
+      retryWrites: true,
+      w: "majority",
+    });
+
     console.log("connection established with database !");
-  } catch (err) {
-    console.log("error in establishing connection with database ");
-    console.log(err.message);
+  } catch (error) {
+    console.error("DB connection failed ", error);
     process.exit(1);
   }
 };
+
 module.exports = dbConnect;
